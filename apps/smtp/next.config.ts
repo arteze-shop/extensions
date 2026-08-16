019a7d33ae699a58af40a5555150297f61a7f01a
khalilcodes <khalilali.j@gmail.com>
2026-08-17 03:30:58 +0400

    [prod]: add vercel services and app base paths

--- apps/smtp/next.config.ts
+++ apps/smtp/next.config.ts
@@ -2,6 +2,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/smtp",
   reactStrictMode: true,
   transpilePackages: [
     "@saleor/apps-otel",
